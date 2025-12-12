f0bc69df8fd00ce7746bc753a1e4fe589e176ddf
Dolly Kukreja <Dolly.Kukreja@mrisoftware.com>
2025-12-12 17:57:29 +0530

    Added Backend Again

---
+++ CRM-Backend/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")] // 🔒 Only Admins can access
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("dashboard")]
+        public IActionResult GetDashboard()
+        {
+            return Ok(new { message = "Welcome Admin!" });
+        }
+    }
+}
---
+++ CRM-Backend/Models/Dtos/Customerdtos.cs
@@ -0,0 +1,33 @@
+﻿namespace CRMBackend.Models.Dtos
+{
+
+    public class ContactDto
+    {
+        public string Type { get; set; } = ""; // "Email" or "Phone"
+        public string Value { get; set; } = "";
+    }
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public string Email { get; set; }
+        //public string Phone { get; set; }
+
+        public string Address { get; set; }
+        public string Company { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<ContactDto> Contacts { get; set; } = new();
+    }
+
+    public class CreateCustomerDto
+    {
+        public string Name { get; set; } = string.Empty;
+        //public string Email { get; set; } = string.Empty;
+        //public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public List<ContactDto> Contacts { get; set; } = new();
+    }
+
+    public class UpdateCustomerDto : CreateCustomerDto { }
+}

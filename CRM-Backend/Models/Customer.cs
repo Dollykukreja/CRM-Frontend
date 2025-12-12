9d3bdccabb6c2aa355c3d3df6fbbcef861fdd207
Dolly Kukreja <Dolly.Kukreja@mrisoftware.com>
2025-12-12 17:37:46 +0530

    Added CRM-Backend

---
+++ CRM-Backend/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using CRM_Backend.Models;
+using System;
+
+namespace CRMBackend.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        //public string Email { get; set; } = string.Empty;
+        //public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<CustomerContact> Contacts { get; set; } = new List<CustomerContact>();
+    }
+}
---
+++ CRM-Backend/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRMBackend.Models
+{
+    public class User
+    {
+        [Key] 
+        public int Id { get; set; }   // Primary Key
+        public string Username { get; set; }     // Primary Key by convention
+        public string Password { get; set; }
+        
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Role { get; set; } 
+    }
+}
